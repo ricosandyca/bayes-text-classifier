11748583cf86eb5e7ed9c66cba0e483c06dd5d6e
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2019-11-28 17:44:03 +0700

    Create express server

---
+++ src/config/app.ts
@@ -0,0 +1,5 @@
+export default {
+  name: 'Bayes text classifier',
+  port: process.env.PORT || 4000,
+  mode: process.env.NODE_ENV || 'development'
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World')
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+import * as config from './config'
+
+const app = express()
+const { port: PORT } = config.app
+const { uri: DBUri, options: DBOptions } = config.database
+
+mongoose
+  .connect(DBUri, DBOptions)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(console.log)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen(PORT, () => console.log('Server running on port', PORT))
